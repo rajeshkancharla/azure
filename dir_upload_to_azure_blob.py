ea65dbe5e6f11b06d3ec24c2c5490d9155d24277
Rajesh Kancharla <rajesh.kancharla@outlook.com>
2022-06-05 19:04:58 +1000

    Create dir_upload_to_azure_blob.py

---
+++ dir_upload_to_azure_blob.py
@@ -0,0 +1,38 @@
+from azure.storage.blob import BlobServiceClient
+import os
+# Install the following package before running this program
+# pip install azure-storage-blob
+
+def upload_data_to_adls():
+    """
+    Function to upload local directory to ADLS
+    :return:
+    """
+    # Azure Storage connection string
+    connect_str = "DefaultEndpointsProtocol=https;AccountName=rkdatalakestorageaccount;AccountKey=VucHl2AZHK5fGsIeVyOvwO7w1z3NXK9dh1MidauzYz6aE+Hq9MKnRpHKanpnzEOBW8bdWwpZPYas+ASt69T7cA==;EndpointSuffix=core.windows.net"
+    # Name of the Azure container
+    container_name = "data"
+    # The path to be removed from the local directory path while uploading it to ADLS
+    path_to_remove = "C:\\Users\\rk\\"
+    # The local directory to upload to ADLS
+    local_path = "C:\\Users\\rk\\test"
+    
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    # The below code block will iteratively traverse through the files and directories under the given folder and uploads to ADLS.
+    for r, d, f in os.walk(local_path):
+        print("r", r)
+        print("d", d)
+        print("f", f)
+        if f:
+            for file in f:
+                file_path_on_azure = os.path.join(r, file).replace(path_to_remove, "")
+                file_path_on_local = os.path.join(r, file)
+                blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_path_on_azure)
+                with open(file_path_on_local, "rb") as data:
+                    blob_client.upload_blob(data)
+                    print("uploading file —->", file_path_on_local)
+
+
+if __name__ == '__main__':
+    # invoking the upload_data_to_adls() function.
+    upload_data_to_adls()
